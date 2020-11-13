0f6a4831c22b63f502b4336957f0012f179b447c
Jipok <braaga@inbox.ru>
2020-11-13 19:33:34 +0500

    first commit

---
+++ login-notify.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+[[ "$PAM_SERVICE" = "cron" ]] && exit 0
+#[[ "$PAM_TYPE" = "close_session" ]] && exit 0
+
+apiToken='YOUR_BOT_TOKEN'
+chatId='CHAT_ID_TO_SEND'
+
+message="${HOSTNAME}: $PAM_RHOST $PAM_TYPE <b>$PAM_USER</b> via ${PAM_TTY}"
+
+curl -s                  \
+    -X POST              \
+    -d text="$message"   \
+    -d chat_id=$chatId   \
+    -d parse_mode="HTML" \
+    https://api.telegram.org/bot$apiToken/sendMessage
